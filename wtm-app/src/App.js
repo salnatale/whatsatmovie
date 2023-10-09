4be1c141ccb626db659577986966b247f6022979
Salvatore Natale <salnatale@Salvatores-MacBook-Pro.local>
2023-10-09 19:39:04 +1300

    second comit

--- wtm-app/src/App.js
+++ wtm-app/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [response, setResponse] = useState('');
   const [success, setSuccess] = useState(true);
 
   const [inputText, setInputText] = useState('');
