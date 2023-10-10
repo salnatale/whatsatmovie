27e202f57708d01729cfdca8088dac22518db15d
Salvatore Natale <salnatale@Salvatores-MacBook-Pro.local>
2023-10-10 21:35:08 +1300

    update

--- wtm-app/src/components/header.js
+++ wtm-app/src/components/header.js
@@ -59,21 +59,21 @@ function Header() {
             <div class="menu-bg" id="menu-bg"></div>
 
             <div className='sliding-text'>
-                {/* <span>There once was a ship that put to sea
-            The name of the ship was the Billy O' Tea
-            The winds blew up, her bow dipped down
-            Oh blow, my bully boys, blow ...  Soon may the
-            Wellerman come
-            To bring us sugar and tea and rum
-            One day, when the tonguing is done
-            We'll take our leave and go ...
-            She'd not been two weeks from shore
-            When down on her a right whale bore
-            The captain called all hands and swore
-            He'd take that whale in tow ... Soon may the Wellerman come
-            To bring us sugar and tea and rum
-            One day, when the tonguing is done
-            We'll take our leave and go</span> */}
+                <span>There once was a ship that put to sea
+                    The name of the ship was the Billy O' Tea
+                    The winds blew up, her bow dipped down
+                    Oh blow, my bully boys, blow ...  Soon may the
+                    Wellerman come
+                    To bring us sugar and tea and rum
+                    One day, when the tonguing is done
+                    We'll take our leave and go ...
+                    She'd not been two weeks from shore
+                    When down on her a right whale bore
+                    The captain called all hands and swore
+                    He'd take that whale in tow ... Soon may the Wellerman come
+                    To bring us sugar and tea and rum
+                    One day, when the tonguing is done
+                    We'll take our leave and go</span>
             </div>
             <a href="/">
                 <img className="site-logo" src={logo} alt="Logo" />
