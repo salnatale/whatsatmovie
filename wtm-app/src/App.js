daa640b9b48a50a71800621c5c6eccf21821c798
Salvatore Natale <salnatale@Salvatores-MacBook-Pro.local>
2023-10-10 20:25:44 +1300

    change structure

--- wtm-app/src/App.js
+++ wtm-app/src/App.js
@@ -3,17 +3,25 @@ import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import MainContent from './components/MainContent.js';
 
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
+
 import './App.css';
 import logo from './logo.png';
 
 
 function App() {
     return (
-        <div className="app">
-            <Header logoSrc={logo} />
-            <MainContent />
-            <Footer />
-        </div>
+        <Router>
+            <div className="app">
+                <Header logoSrc={logo} />
+                <MainContent />
+                <Footer />
+            </div>
+        </Router>
 
     );
 }
