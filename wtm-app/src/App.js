b90e63c69c3624b1abf4b2631cb3df6a67db3582
Salvatore Natale <salnatale@Salvatores-MacBook-Pro.local>
2023-10-10 17:47:33 +1300

    comment sliding text for time being.

--- wtm-app/src/App.js
+++ wtm-app/src/App.js
@@ -126,7 +126,7 @@ function App() {
           <img className="site-logo" src={logo} alt="Logo" />
         </a>
         <div className='sliding-text'>
-          <span>There once was a ship that put to sea
+          {/* <span>There once was a ship that put to sea
             The name of the ship was the Billy O' Tea
             The winds blew up, her bow dipped down
             Oh blow, my bully boys, blow ...  Soon may the
@@ -140,7 +140,7 @@ function App() {
             He'd take that whale in tow ... Soon may the Wellerman come
             To bring us sugar and tea and rum
             One day, when the tonguing is done
-            We'll take our leave and go</span>
+            We'll take our leave and go</span> */}
         </div>
         <i className="fas fa-share-alt" onClick={handleShareClick}></i>
       </header>
