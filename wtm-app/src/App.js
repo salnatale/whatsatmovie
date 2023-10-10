17b042a85b68ac667f75e7e10b489b9ad73234fd
Salvatore Natale <salnatale@Salvatores-MacBook-Pro.local>
2023-10-10 19:54:45 +1300

    whole structure change

--- wtm-app/src/App.js
+++ wtm-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
-import MainContent from './components/maincontent.js';
+import MainContent from './components/MainContent.js';
 
 import './App.css';
 import logo from './logo.png';
