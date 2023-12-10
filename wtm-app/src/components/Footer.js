8c344209f59c44b86a403f323262e2f376855d94
Salvatore Natale <salnatale@Salvatores-MacBook-Pro.local>
2023-12-10 16:01:31 -0500

    added back dono

--- wtm-app/src/components/Footer.js
+++ wtm-app/src/components/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
                         <input type="hidden" name="currency_code" value="USD" />
                         <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
                         <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-                    </form>  comment out dono for now
+                    </form>
                 </div>
             </div >
         </footer>
